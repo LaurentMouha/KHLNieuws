152bcdd6541b7768031d164d789126049897e894
Laurent Mouha <lmouha@gmail.com>
2012-02-24 14:53:37 +0100

    test

---
+++ src/be/khleuven/nieuws/KHLNieuwsActivity.java
@@ -0,0 +1,84 @@
+package be.khleuven.nieuws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KHLNieuwsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+    try {
+        HttpGet httpget = new HttpGet("https://portal.sun.com/portal/dt");
+
+        HttpResponse response = httpclient.execute(httpget);
+        HttpEntity entity = response.getEntity();
+
+        System.out.println("Login form get: " + response.getStatusLine());
+        EntityUtils.consume(entity);
+
+        System.out.println("Initial set of cookies:");
+        List<Cookie> cookies = httpclient.getCookieStore().getCookies();
+        if (cookies.isEmpty()) {
+            System.out.println("None");
+        } else {
+            for (int i = 0; i < cookies.size(); i++) {
+                System.out.println("- " + cookies.get(i).toString());
+            }
+        }
+
+        HttpPost httpost = new HttpPost("https://portal.sun.com/amserver/UI/Login?" +
+                "org=self_registered_users&" +
+                "goto=/portal/dt&" +
+                "gotoOnFail=/portal/dt?error=true");
+
+        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+        nvps.add(new BasicNameValuePair("IDToken1", "username"));
+        nvps.add(new BasicNameValuePair("IDToken2", "password"));
+
+        httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+
+        response = httpclient.execute(httpost);
+        entity = response.getEntity();
+
+        System.out.println("Login form get: " + response.getStatusLine());
+        EntityUtils.consume(entity);
+
+        System.out.println("Post logon cookies:");
+        cookies = httpclient.getCookieStore().getCookies();
+        if (cookies.isEmpty()) {
+            System.out.println("None");
+        } else {
+            for (int i = 0; i < cookies.size(); i++) {
+                System.out.println("- " + cookies.get(i).toString());
+            }
+        }
+
+    } finally {
+        // When HttpClient instance is no longer needed,
+        // shut down the connection manager to ensure
+        // immediate deallocation of all system resources
+        httpclient.getConnectionManager().shutdown();
+    }
+
+}
